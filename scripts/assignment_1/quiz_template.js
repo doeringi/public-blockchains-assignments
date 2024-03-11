d80c6d19a2144b7222e2c8f7201aca221ff30a4d
doeringi <doeringi@hs-pforzheim.de>
2024-03-11 17:35:50 +0100

    add code for assignment 1

--- scripts/assignment_1/quiz_template.js
+++ scripts/assignment_1/quiz_template.js
@@ -13,11 +13,14 @@ const ethers = require("ethers");
 
 // Adjust path to your .env file.
 const pathToDotEnv = path.join(__dirname, "..", "..", ".env");
-// console.log(pathToDotEnv);
+console.log(pathToDotEnv);
+
 require("dotenv").config({ path: pathToDotEnv });
 
-const { getUserAnswer, extractQuestion } =
-    require(path.join(__dirname, "quiz_helper.js"));
+const { getUserAnswer, extractQuestion } = require(path.join(
+  __dirname,
+  "quiz_helper.js"
+));
 
 // Create Signer and Contract.
 //////////////////////////////
@@ -28,8 +31,8 @@ const sepoliaUrl = `${process.env.ALCHEMY_SEPOLIA_API_URL}${providerKey}`;
 const sepoliaProvider = new ethers.JsonRpcProvider(sepoliaUrl);
 
 const signer = new ethers.Wallet(
-    process.env.METAMASK_1_PRIVATE_KEY,
-    sepoliaProvider
+  process.env.METAMASK_1_PRIVATE_KEY,
+  sepoliaProvider
 );
 
 const quizABI = require(path.join(__dirname, "quiz_abi"));
@@ -40,16 +43,25 @@ const contractAddress = "0x01FaE6a3E15b8cf2cb89C259b2d6e5bf7cf94782";
 const quizContract = new ethers.Contract(contractAddress, quizABI, signer);
 
 async function main() {
+  // A. Ask question and get a transaction receipt.
+  // Hint: method `askQuestion()`
 
-    // A. Ask question and get a transaction receipt.
-    // Hint: method `askQuestion()`
+  // Your code here.
 
-    // Your code here.
+  try {
+    const response = await quizContract.askQuestion();
+    // console.log(`Transaction Response: ${JSON.stringify(txResponse)}`);
+
+    const receipt = await response.wait();
+    // console.log(`Receipt: ${JSON.stringify(receipt)}`);
 
     // From the transaction receipt we can extract useful information, such as
     // as the question's text and id that were stored in the logs
     // (we will understand logs in detail later in the course).
     const { text, id } = extractQuestion(quizContract, receipt);
+
+    console.log(`The question id is: ${id}`);
+    console.log(`The question is: ${text}`);
 
     // Now YOU answer the question!
     // Capture user input from the terminal.
@@ -59,17 +71,22 @@ async function main() {
     // Hint: method `answerQuestion`.
 
     // Your code here.
+    const tx = await quizContract.answerQuestion(id, userAnswer);
 
     // C. Optional. Verify that the answer is correctly stored.
     // Hint: method `getAnswer(questionId)`
 
     // Your code here.
+    const answer = await quizContract.getAnswer(id);
+    console.log(`The answer is: ${answer}`);
+  } catch (error) {
+    console.error("Error answering question:", error);
+  }
 }
 
-
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
